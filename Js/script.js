7b458161469c2e8319a400ed5937509c731290de
qLex7 <debord.guillaume@hotmail.fr>
2017-05-08 17:31:56 +0200

    Update script.js

--- Js/script.js
+++ Js/script.js
@@ -18,33 +18,22 @@ document.body.onclick=function(e)
 	}
 }
 
-var slideIndex = 1;
-showDivs(slideIndex);
+  var myIndex = 0;
+  carousel();
 
-function plusDivs(n) 
+function carousel() 
 {
-  showDivs(slideIndex += n);
-}
-
-function currentDiv(n) 
-{
-  showDivs(slideIndex = n);
-}
-
-function showDivs(n) 
-{
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) 
-  	{
-  		slideIndex = 1
-  	}    
-  if (n < 1) 
-  	{
-  		slideIndex = x.length
-  	}
-  for (i = 0; i < x.length; i++)
-   {
-     x[i].style.display = "none";  
-   }
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) 
+    {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) 
+      {
+        myIndex = 1
+      }    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 5000); // Change l'image toutes les 5 secondes
 };
